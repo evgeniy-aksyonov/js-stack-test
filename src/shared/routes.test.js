d61517f6f796a2810e572fffaad01b80ab856251
Evgeniy Aksyonov <evgeniy.aksyonov@cloudmade.com>
2017-07-30 22:25:42 +0300

    05. Extending our app with an asynchronous call

---
+++ src/shared/routes.test.js
@@ -0,0 +1,6 @@
+import { helloEndpointRoute } from '../server/routes'
+
+test('helloEndpointRoute', () => {
+  expect(helloEndpointRoute()).toBe('/ajax/hello/:num')
+  expect(helloEndpointRoute(123)).toBe('/ajax/hello/123')
+})
